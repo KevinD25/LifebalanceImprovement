38b00f3d963a354fdc4ac0ad526fe2e60be9f560
Rob Mertens <s100934@ap.be>
2019-10-09 14:47:27 +0200

    Made exercise Tabs + slight layout of tabs + made text editable

--- Project/LifeBalanceImprovement/src/app/tab4/tab4.page.ts
+++ Project/LifeBalanceImprovement/src/app/tab4/tab4.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ExpandableComponent } from '../Items/expandable/expandable.component';
 
 @Component({
   selector: 'app-tab4',
@@ -6,7 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
+  public items: any = [];
+  
 
-  constructor() {}
+  constructor() {
+    this.items = [
+      { expanded: false, templateUrl : "testing 123456789",plab : "Title1", index : 0 },
+      { expanded: false, templateUrl : "testing testing",plab : "Title2", index : 1 },
+      { expanded: false, templateUrl : " 123456789",plab : "Title3",index : 2 }
+    ];
+    
+    
+  }
 
+  expandItem(item): void {
+    if (item.expanded) {
+      item.expanded = false;
+    } else {
+      this.items.map(listItem => {
+        if (item == listItem) {
+          listItem.expanded = !listItem.expanded;
+        } else {
+          listItem.expanded = false;
+        }
+        return listItem;
+      });
+    }
+  }
+  pressButton(item):void
+  {
+  
+    console.log(item.index);
+  }
 }
