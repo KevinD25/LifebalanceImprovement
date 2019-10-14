918e56e572ea48d0a9aa963eff0dfdcb2042a04b
KevinDav <kevin-davis@hotmail.com>
2019-10-14 14:29:25 +0200

    Guard implemented

--- Project/LifeBalanceImprovement/src/app/app-routing.module.ts
+++ Project/LifeBalanceImprovement/src/app/app-routing.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
+  { 
+    path: 'auth', 
+    loadChildren: './auth/auth.module#AuthPageModule' },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  { path: 'happy-exer', loadChildren: './tab4/happy-exer/happy-exer.module#HappyExerPageModule' },
-  { path: 'mindfull-exer', loadChildren: './tab4/mindfull-exer/mindfull-exer.module#MindfullExerPageModule' },
-  { path: 'psycho-edu', loadChildren: './tab4/psycho-edu/psycho-edu.module#PsychoEduPageModule' },
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canLoad: [AuthGuard] 
+  },
+  { 
+    path: 'happy-exer', 
+    loadChildren: './tab4/happy-exer/happy-exer.module#HappyExerPageModule', 
+    canLoad: [AuthGuard] 
+},
+  { 
+    path: 'mindfull-exer', 
+    loadChildren: './tab4/mindfull-exer/mindfull-exer.module#MindfullExerPageModule',
+   canLoad: [AuthGuard] 
+  },
+  { 
+    path: 'psycho-edu', 
+    loadChildren: './tab4/psycho-edu/psycho-edu.module#PsychoEduPageModule', 
+    canLoad: [AuthGuard] 
+},
+  
 
 
 
---
+++ Project/LifeBalanceImprovement/src/app/auth/auth.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.page.html',
+  styleUrls: ['./auth.page.scss']
+})
+export class AuthPage implements OnInit{
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(){}
+
+  onLogin(){
+      this.authService.login();
+  }
+
+}
