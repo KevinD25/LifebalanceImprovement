f441b7b340f1330add8607f9434fefc7c26a7544
Rob Mertens <s100934@ap.be>
2019-10-17 13:19:35 +0200

    Added random exercises showup for every visit

--- Project/LifeBalanceImprovement/src/app/tab4/mindfull-exer/mindfull-exer.page.ts
+++ Project/LifeBalanceImprovement/src/app/tab4/mindfull-exer/mindfull-exer.page.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { CRUDServiceService } from '../../services/crudservice.service';
 
 @Component({
   selector: 'app-mindfull-exer',
   templateUrl: './mindfull-exer.page.html',
   styleUrls: ['./mindfull-exer.page.scss'],
 })
-export class MindfullExerPage implements OnInit {
+export class MindfullExerPage implements OnInit{
 
-  constructor() { }
-
-  ngOnInit() {
+  
+  Exercises : any;
+  Exercise :string;
+  TodaysExercise : string;
+  constructor(private crudService: CRUDServiceService) { }
+ 
+  ngOnInit() 
+  {
+    
+    
+    this.crudService.read_Users("MindfullEx").subscribe(data => {
+ 
+      this.Exercises = data.map(e => {
+        return {
+          ExerciseText: e.payload.doc.data()['Exercise'],
+          
+        };
+      })
+      this.TodaysExercise = this.Exercises[Math.floor(Math.random() * this.Exercises.length)].ExerciseText;
+      console.log(this.Exercises);
+ 
+    });
+    
   }
-
+  CreateRecord() {
+    let record = {};
+    record['Exercise'] = this.Exercise;
+   
+    this.crudService.create_NewUser(record,"MindfullEx").then(resp => {
+      this.Exercise = "";
+     
+      console.log(resp);
+    })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+ 
+ 
 }
