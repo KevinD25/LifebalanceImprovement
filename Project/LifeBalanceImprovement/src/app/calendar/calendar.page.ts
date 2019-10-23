90a7734f34814426d3f6f6ef9ed638354b15d8dd
jaro <jaro.helsen@hotmail.be>
2019-10-23 11:24:17 +0200

    added events

--- Project/LifeBalanceImprovement/src/app/calendar/calendar.page.ts
+++ Project/LifeBalanceImprovement/src/app/calendar/calendar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-calendar',
@@ -7,17 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarPage implements OnInit {
 
-  eventSource;
+  eventSource = [];
 
   calendar = {
     mode: 'month',
     currentDate: new Date(),
   };
   selectedDate = new Date();
+  titleEvent: string;
+  firstTime: string = '';
+  lastTime: string = '';
+  fullDay: boolean =  false;
+  addEvent: boolean;
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {
+    this.addEvent = false;
+    this.db.collection(`Events`).snapshotChanges().subscribe(colSnap => {
+      this.eventSource = [];
+      colSnap.forEach(snap => {
+        let event:any = snap.payload.doc.data();
+        event.id = snap.payload.doc.id;
+        event.startTime = event.startTime.toDate();
+        event.endTime = event.endTime.toDate();
+        console.log(event);
+        this.eventSource.push(event);
+      });
+    });
+   }
 
   ngOnInit() {
+  }
+
+  addNewEvent() {
+    let now = this.selectedDate;
+    let start = new Date(this.selectedDate.getFullYear(), this.selectedDate.getMonth(), this.selectedDate.getDate());
+    let end = new Date(this.selectedDate.getFullYear(), this.selectedDate.getMonth(), this.selectedDate.getDate());
+    if (this.fullDay){
+      start.setHours(0, 0, 0, 0);
+      end.setHours(23, 59, 59, 999);
+    } else {
+      let hours = this.firstTime.split(':', 2);
+      start.setHours(+hours[0]);
+      start.setMinutes(+hours[1]);
+      hours = this.lastTime.split(':', 2);
+      end.setHours(+hours[0]);
+      end.setMinutes(+hours[1]);
+    }
+    
+
+    let event = {
+      title: this.titleEvent,
+      startTime: start,
+      endTime: end,
+      allDay: this.fullDay
+    };
+    console.log(event);
+
+    this.db.collection(`Events`).add(event);
+    this.addEvent = false;
+  }
+
+  EventAddButtonPressed(){
+    this.addEvent = true;
   }
 
   onViewTitleChanged(title) {
@@ -31,6 +83,7 @@ export class CalendarPage implements OnInit {
   onTimeSelected(ev) {
     console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
       (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+    this.selectedDate = ev.selectedTime;
   }
 
   onCurrentDateChanged(event: Date) {
--- Project/LifeBalanceImprovement/src/environments/environment.ts
+++ Project/LifeBalanceImprovement/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB3L72vY1gjMfoNBkm2Z5UYPiHncj3pOvE',
+    authDomain: 'lifebalanceimprovement.firebaseapp.com',
+    databaseURL: 'https://lifebalanceimprovement.firebaseio.com',
+    projectId: 'lifebalanceimprovement',
+    storageBucket: 'lifebalanceimprovement.appspot.com',
+    messagingSenderId: '212602605399',
+    appId: '1:212602605399:web:140807689d6334fb6f7812',
+    measurementId: 'G-K051MYHEVD'
+  }
 };
 
 /*
