f6f72f541ba66e20dce011e27be02c3cb8e74ace
Scazi <kevin-davis@hotmail.com>
2019-11-13 20:25:35 +0100

    checkpoint

--- Project/LifeBalanceImprovement/src/app/tab1/tab1.page.ts
+++ Project/LifeBalanceImprovement/src/app/tab1/tab1.page.ts
@@ -7,12 +7,42 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public quotes : any = [];
+  quote : string;
+  currentday : number;
+  currentdaystring: string;
+  quotenumber : number;
 
+  constructor() {
+
+    this.quotes = [
+      {text: "‘Dit is wat ik heb geleerd: Dat iedereen een talent heeft, origineel is en iets belangrijks te zeggen heeft.’ Brenda Ueland, Amerikaans schrijfster"},
+      {text : "Bekijk vandaag de anderen op deze manier: ze hebben een talent en iets belangrijks te zeggen. Ontdek het."},
+      {text: "‘Ik wil alles worden wat ik kan worden.’ Katherine Mansfield, Nieuw-Zeelands schrijfster"},
+      {text : "Ga vandaag op zoek naar dat ene talent van je dat nog wat verborgen zit. Geef het een kans om te groeien."},
+      {text: "‘Twijfel is niet het tegenovergestelde van geloof, het is een onderdeel van geloof.’ Paul Tillich, Duits theoloog"}
+    ]
+
+    this.getCurrentDate();
+    console.log(this.currentday);
+    console.log(this.quotes.length);
+    this.generateQotd();
+    console.log(this.quotenumber);
+  }
+
+   
   
   //Random tip of the day based on day. Use day to generate N number to show specific qotd that changes every day.
   generateQotd(){
+      this.quotenumber = this.currentday % this.quotes.length;
+      this.quote = this.quotes[this.quotenumber].text;
+      console.log(this.quote);
+  }
 
+  getCurrentDate(){
+    this.currentdaystring = new Date().getDate().toString() + new Date().getMonth().toString() + new Date().getFullYear().toString(); 
+    this.currentday = parseInt(this.currentdaystring);
+    this.currentday = 15112019;
   }
 
 }
