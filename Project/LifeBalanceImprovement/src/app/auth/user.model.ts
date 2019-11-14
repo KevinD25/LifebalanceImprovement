130fc802ee86c7a7a2a3edbbfb2d83387f383a56
Scazi <kevin-davis@hotmail.com>
2019-11-14 15:02:24 +0100

    Fixed tab auth security

--- Project/LifeBalanceImprovement/src/app/auth/user.model.ts
+++ Project/LifeBalanceImprovement/src/app/auth/user.model.ts
@@ -14,6 +14,7 @@ get tokenDuration(){
     if(!this.token){
         return 0;
     }
+    
     return  this.tokenExpirationDate.getTime() - new Date().getTime();
 }
 }
--- Project/LifeBalanceImprovement/src/app/tabs/tabs.router.module.ts
+++ Project/LifeBalanceImprovement/src/app/tabs/tabs.router.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+              import('../tab1/tab1.module').then(m => m.Tab1PageModule),
+              canLoad: [AuthGuard]
           }
         ]
       },
@@ -24,7 +25,8 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+              import('../tab2/tab2.module').then(m => m.Tab2PageModule),
+              canLoad: [AuthGuard]
           }
         ]
       },
@@ -34,7 +36,8 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import('../tab3/tab3.module').then(m => m.Tab3PageModule),
+              canLoad: [AuthGuard]
           }
         ]
       },
@@ -44,7 +47,8 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab4/tab4.module').then(m => m.Tab4PageModule)
+              import('../tab4/tab4.module').then(m => m.Tab4PageModule),
+              canLoad: [AuthGuard]
           }
         ]
       },
@@ -54,7 +58,8 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab5/tab5.module').then(m => m.Tab5PageModule)
+              import('../tab5/tab5.module').then(m => m.Tab5PageModule),
+              canLoad: [AuthGuard]
           }
         ]
       },
